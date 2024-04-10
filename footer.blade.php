8858d9f6d1147e17a669cd6df8522cfca9e7ad82
Rajon Ahmed <77260323+rajonahmed1984@users.noreply.github.com>
2024-04-10 11:21:07 +0600

    nav
    nav

--- footer.blade.php
+++ footer.blade.php
@@ -1,6 +1,33 @@
     
         
-        
+        <div class="fixed-bottom" style="">
+            <div class="navigation-list">
+                <a type="button" class="btn position-relative" href="/didartel/notification.php">
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                        99+
+                        <span class="visually-hidden">unread messages</span>
+                    </span>
+                    <i class="bi bi-bell"></i>
+                    <p>Notification</p>
+                </a>
+                <a href="/didartel/reports.php" class="navigation-icon">
+                    <i class="bi bi-graph-up-arrow"></i>
+                    <p>Reports</p>
+                </a>
+                <a href="/didartel/home.php" class="navigation-icon active">
+                    <i class="bi bi-house-door-fill"></i>
+                    <p>Home</p>
+                </a>
+                <a href="/didartel/resellers.php" class="navigation-icon">
+                    <i class="bi bi-people"></i>
+                    <p>Reseller</p>
+                </a>
+                <a class="navigation-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <i class="bi bi-text-right"></i>
+                    <p>Menu</p>
+                </a>
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <script src="js/scrolled.js"></script>
