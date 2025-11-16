32a9100ef2dbfa00770dd9621a6428117f28c990
mostofaadnan <msa.cse.metro@gmail.com>
2025-11-17 00:25:16 +0600

    c

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -14,7 +14,7 @@
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/js/admin/app.js'])
-    @vite(['resources/js/admin/app.js'])
+    @vite(['resources/css/admin.css'])
     @stack('styles')
 </head>
 
